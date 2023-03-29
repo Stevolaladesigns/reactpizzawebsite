8e50d83dd914fb6612bf42ba5e57b014202d49fb
Stephen  Kwasi Onumah <onumahbaba@gmail.com>
2023-03-29 11:57:27 +0000

    react pizza app

---
+++ src/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () =>{
+   return (
+<footer className="footer">
+  <div className="container">
+    <div className="row">
+      <p className="text-center"> &copy; 2023 All Rights Reserved  </p>
+      
+
+    </div>
+  </div>
+</footer>
+
+
+   );
+
+
+
+
+}
+export default Footer;
